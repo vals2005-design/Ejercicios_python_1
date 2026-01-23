de3acfd6c04af85a71e0d61d98b1ad335ff28615
vals2005 <martinezbaez.@gmail.com>
2026-01-23 10:36:00 -0600

    mi segundo comint

---
+++ ejercicio_04.py
@@ -0,0 +1,15 @@
+# dados dos numeros muestra la suma,
+# resta, multiplicacion y divicion 
+
+num_1 = int(input("ingresa un numero: "))
+num_2 = int(input("ingresa otro numero: "))
+
+suma = num_1 + num_2
+resta = num_1 - num_2
+multi = num_1 * num_2
+divi = num_1 / num_2
+
+print('suma:', suma)
+print('resta:', resta)
+print('multiplicacion:', multi)
+print('divicion:', divi)
